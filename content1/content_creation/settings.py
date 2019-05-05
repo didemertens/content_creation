2fd7ccee3fc54ae6be5710ce004ca2edb43503bb
Dide Mertens <d.g.j.mertens@gmail.com>
2019-05-05 15:12:11 +0200

    small changes, align elements

--- content1/content_creation/settings.py
+++ content1/content_creation/settings.py
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # sass
-    'sass_processor',
     # local
     'basic_content',
 ]
@@ -126,10 +124,10 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
 ]
 
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-)
+# STATICFILES_FINDERS = (
+#     'django.contrib.staticfiles.finders.FileSystemFinder',
+#     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+# )
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
